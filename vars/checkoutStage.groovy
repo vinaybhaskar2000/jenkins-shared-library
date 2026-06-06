674e654dc1cd3ce23c02538456a74c430d165549
VINAY BHASKAR <41627180+vinaybhaskar2000@users.noreply.github.com>
2026-06-06 13:01:59 +0530

    Stage creation INSIDE shared library_checkoutStage --->  checkoutStage.groovy

---
+++ vars/checkoutStage.groovy
@@ -0,0 +1,10 @@
+def call(String repoUrl, String branchName='main')
+{
+  stage('Checkout')
+  {
+    echo "Checking out Code From  ${repoUrl}"
+    git branch: branchName,
+      url = repoUrl
+  }
+
+}
